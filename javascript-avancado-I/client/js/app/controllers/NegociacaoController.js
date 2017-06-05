4e000541c372abbdd54624df125b1e371fa7e72c
Luis Garcia <luisgarcia@plusoft.com.br>
2017-06-05 14:29:57 -0300

    Arrow function

--- javascript-avancado-I/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-I/client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,44 @@
 // Classe controller de Negociacao (acoes tela)
 class NegociacaoController {
 
+    // Construtor
+    construct() {
+        // Recupera dados do form somente uma vez (performatico)
+        let $ = document.querySelector.bind(document); // Copiando jQuery
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+
     // Metodos
     adiciona(event) {
         event.preventDefault(); // Previne reload tela
         
-        // Recupera dados do form
-        let $ = document.querySelector.bind(document); // Copiando jQuery
-        let inputData = $("#data");
-        let inputQuantidade = $("#quantidade");
-        let inputValor = $("#valor");
+        // Conversao data 1
+        // let data = new Date(this._inputData.value.replace(/-/g, ','));
+        
+        // Conversao data 2
+        /*let data = new Date(
+            this._inputData.value
+                .split('-')
+                .map(function(item, indice) {
+                    return item - indice % 2;
+                })
+        );*/
+
+        // Conversao data 3 - Arrow function
+        let dataConvertida = new Date(
+            this._inputData.value // String
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        );
+
+        // Instancia do modelo
+        let negociacao = new Negociacao(
+            dataConvertida,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
 
     }
 
