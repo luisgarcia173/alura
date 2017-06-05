59a54632ecad881e3528ef9b36c540a588a213b3
Luis Garcia <luisgarcia@plusoft.com.br>
2017-06-05 15:10:33 -0300

    Template String

--- javascript-avancado-I/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-I/client/js/app/helpers/DateHelper.js
@@ -11,7 +11,8 @@ class DateHelper {
     }
 
     static dataParaTexto(data) {
-        return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+        //return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`; // Usando TemplateString
     }
 
 }
