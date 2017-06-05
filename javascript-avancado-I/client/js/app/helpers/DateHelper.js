eed935e0401f57235c25861aa1ca674c3c1d0468
Luis Garcia <luisgarcia@plusoft.com.br>
2017-06-05 15:15:23 -0300

    Validacao data

--- javascript-avancado-I/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-I/client/js/app/helpers/DateHelper.js
@@ -6,13 +6,16 @@ class DateHelper {
         throw new Error("DateHelper nao pode ser instanciada.");
     }
 
-    static textoParaData(texto) {
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
-    }
-
     static dataParaTexto(data) {
         //return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`; // Usando TemplateString
     }
 
+    static textoParaData(texto) {
+        let regexData = /\d{4}-\d{2}-\d{2}/;
+        if(!regexData.test(texto)) {
+            throw new Error("Formato de data esperado eh aaaa-mm-dd");
+        }
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
 }
