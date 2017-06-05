6f167d4112e04789e81ef836c8f02fd02090e2b2
Luis Garcia <luisgarcia@plusoft.com.br>
2017-06-05 15:05:13 -0300

    Inibindo construtor Helper

--- javascript-avancado-I/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-I/client/js/app/helpers/DateHelper.js
@@ -1,6 +1,11 @@
 // Classe de conversão de Data
 class DateHelper {
 
+    // Força erro quando tentar instanciar
+    constructor() {
+        throw new Error("DateHelper nao pode ser instanciada.");
+    }
+
     static textoParaData(texto) {
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
