d596b08015544668463add12e58fc216eaeeb179
Luis Garcia <luisgarcia173@gmail.com>
2017-08-30 22:28:52 -0300

    Adicionado Ajax para consumir servico rest GET usando js puro com XHR

--- javascript-avancado-II/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-II/client/js/app/controllers/NegociacaoController.js
@@ -34,6 +34,38 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importa() {
+        // Abre objeto ajax 
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", 'negociacoes/semana');
+
+        // Configuracao
+        xhr.onreadystatechange = () => {
+            /*
+            0: requisicao nao iniciada
+            1: conexao estabelecida
+            2: requisicao recebida
+            3: processando requisicao
+            4: requisicao concluida
+            */
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) { // HTTP code de sucesso
+                    // Parse json do array
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = "Importação das negociaçōes realizada com sucesso!"
+                } else {
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = "Não foi possível obter as negociaçōes da semana."
+                }
+            }
+        };
+
+        // Request servico
+        xhr.send();
+    }
+
     apaga() {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = "Negociações apagadas com sucesso!";
