0f4e2fa8ae73bab265c8d1dcb048bdcc7a0a180b
Luis Garcia <luisgarcia@plusoft.com.br>
2017-08-31 14:58:57 -0300

    Aplicado Pattern Module na connection factory

--- javascript-avancado-III/client/js/app/services/ConnectionFactory.js
+++ javascript-avancado-III/client/js/app/services/ConnectionFactory.js
@@ -1,49 +1,58 @@
-// Global variables
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe'
+// Transforma classe em um modulo usando funcao anonima (Design Pattern: Module)
+var ConnectionFactory = (function(){
 
-
-// Classe responsavel por controlar conexao banco
-class ConnectionFactory {
-
-    constructor() {
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+    // Scope variables
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe'
+    var connection = null;
+    
+    // Classe responsavel por controlar conexao banco
+    return class ConnectionFactory {
+    
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+    
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+    
+                // Cria Database (nome, versao)
+                let openRequest = window.indexedDB.open(dbName, version);
+    
+                // Quando criado ou alterado (baseado na versao)
+                openRequest.onupgradeneeded = (e) => {
+                    // Crio as stores
+                    ConnectionFactory._createStores(e.target.result);
+                };
+    
+                // Quando iniciado o DB
+                openRequest.onsuccess = (e) => {
+                    if(!connection){
+                        connection = e.target.result;
+                    }
+                    resolve(connection);
+                };
+    
+                // Em caso de erro
+                openRequest.onerror = (e) => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+    
+            });
+        }
+    
+        static _createStores(connection) {
+            stores.forEach(store => {
+                // Verifica se tabela existe e remove
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store)
+                }
+                // Cria tabela com auto incremento
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
+    
     }
-
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-
-        // Cria Database (nome, versao)
-        let openRequest = window.indexedDB.open(dbName, version);
-
-        // Quando criado ou alterado (baseado na versao)
-        openRequest.onupgradeneeded = (e) => {
-            console.log('Cria ou atualiza um banco já existente.');
-
-            let minhaConnection = e.target.result;
-
-            // Verifica se tabela existe e remove
-            if(minhaConnection.objectStoreNames.contains('negociacoes')){
-                minhaConnection.deleteObjectStore('negociacoes');    
-            }
-
-            // Cria tabela com auto incremento
-            minhaConnection.createObjectStore('negociacoes', {autoIncrement: true});
-        };
-
-        // Quando iniciado o DB
-        openRequest.onsuccess = (e) => {
-            console.log('Conexão obtida com sucesso!');
-            connection = e.target.result;
-        };
-
-        // Em caso de erro
-        openRequest.onerror = (e) => {
-            console.log(e.target.error);
-        };
-
-        });
-    }
-
-}
+})();
