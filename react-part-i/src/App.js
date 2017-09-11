fcfbe2a5a42f6a39600114c2e555f84e6315992a
Luis Garcia <luisgarcia@plusoft.com.br>
2017-09-11 16:22:00 -0300

    Atualizando a listagem apos inserção

--- react-part-i/src/App.js
+++ react-part-i/src/App.js
@@ -44,8 +44,9 @@ class App extends Component {
       type: 'post',
       data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
       success:function(resposta){
+        this.setState({lista: resposta});
         console.log("enviado com sucesso");
-      },
+      }.bind(this),
       error:function(resposta){
         console.log("erro");
       }
