0b927b3b32158a899bdaac453303e0f4f587e075
Luis Garcia <luisgarcia@plusoft.com.br>
2017-09-12 14:37:56 -0300

    Adicionando Rotas

--- react-part-i/src/index.js
+++ react-part-i/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* 
+  BrowserRouter: rotas dinamicas
+  HashRouter: rotas estaticas
+*/
+ReactDOM.render(
+  (
+  <BrowserRouter> 
+    <Switch>
+      <Route path="/" component={App}/>
+      <Route path="/autor"/>
+      <Route path="/livro"/>
+    </Switch>
+  </BrowserRouter>
+  ),
+  document.getElementById('root')
+);
 registerServiceWorker();
