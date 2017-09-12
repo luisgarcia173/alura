25bc5428086a81a54e11ab520436a5402cf42145
Luis Garcia <luisgarcia@plusoft.com.br>
2017-09-12 12:13:18 -0300

    Refatorando cadastro de autor

--- react-part-i/src/App.js
+++ react-part-i/src/App.js
@@ -3,73 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
-import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
+import {FormularioAutor, TabelaAutores} from './Autor';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      lista : [],
-      nome: '', email: '', senha: ''
-    };
-    this.enviaForm = this.enviaForm.bind(this); // associa o this do react dentro desse metodo
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount() {
-    // Executa após o render ser executado
-    $.ajax({
-      url:"http://cdc-react.herokuapp.com/api/autores",
-      dataType: 'json',
-      success:function(resposta){
-        this.setState({lista:resposta}); // chama o render novamente
-      }.bind(this)
-    });
-  }
-
-  componentWillMount() {
-    // Executa antes do render ser executado
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault();
-
-    $.ajax({
-      url:"http://cdc-react.herokuapp.com/api/autores",
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
-      success:function(resposta){
-        this.setState({lista: resposta});
-        console.log("enviado com sucesso");
-      }.bind(this),
-      error:function(resposta){
-        console.log("erro");
-      }
-    });
-  }
-
-  setNome(evento) { this.setState({nome: evento.target.value}); }
-  setEmail(evento) { this.setState({email: evento.target.value}); }
-  setSenha(evento) { this.setState({senha: evento.target.value}); }
-
   render() {
     return (
-      /*<div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>*/
       <div id="layout">
       {/*<!-- Menu toggle -->*/}
         <a href="#menu" id="menuLink" className="menu-link">
@@ -101,47 +40,8 @@ class App extends Component {
               <p>
                   Each Author is responsible for explain their published work, it means you only can registrate a new Author if his has at least a book published.
               </p>
-      
-              {/* Form */}
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                  <fieldset>
-                    <legend>Author Data</legend>
-                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" placeholder="Nome"/>                                              
-                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" placeholder="Email"/>                                              
-                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" placeholder="Senha"/>                                                                      
-                    <BotaoSubmitCustomizado label="Gravar"/>
-                  </fieldset>
-                </form>
-              </div> 
-
-              {/* Table */}
-              <div>
-                <fieldset>
-                  <legend>Author's List</legend>            
-                  <table className="pure-table pure-u-5-5">
-                    <thead>
-                      <tr>
-                        <th width="75%">Name</th>
-                        <th>Email</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        this.state.lista.map(function(autor) {
-                          return (
-                            <tr key={autor.id}>
-                              <td>{autor.nome}</td>                
-                              <td>{autor.email}</td>                
-                            </tr>
-                          );
-                        })
-                      }
-                    </tbody>
-                  </table>
-                </fieldset>
-              </div>     
-
+              <FormularioAutor />
+              <TabelaAutores />
             </div>
         </div>
     </div>
---
+++ react-part-i/src/Autor.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
+
+export class FormularioAutor extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            lista : [],
+            nome: '', email: '', senha: ''
+        };
+        this.enviaForm = this.enviaForm.bind(this); // associa o this do react dentro desse metodo
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    setNome(evento) { this.setState({nome: evento.target.value}); }
+    setEmail(evento) { this.setState({email: evento.target.value}); }
+    setSenha(evento) { this.setState({senha: evento.target.value}); }
+
+    enviaForm(evento) {
+        evento.preventDefault();
+    
+        $.ajax({
+            url:"http://cdc-react.herokuapp.com/api/autores",
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'post',
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+            success:function(resposta){
+                this.setState({lista: resposta});
+                console.log("enviado com sucesso");
+            }.bind(this),
+            error:function(resposta){
+                console.log("erro");
+            }
+        });
+    }
+
+    render() {
+        return(
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                    <fieldset>
+                        <legend>Author Data</legend>
+                        <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" placeholder="Nome"/>                                              
+                        <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" placeholder="Email"/>                                              
+                        <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" placeholder="Senha"/>                                                                      
+                        <BotaoSubmitCustomizado label="Gravar"/>
+                    </fieldset>
+                </form>
+            </div> 
+        );
+    }
+    
+
+}
+export class TabelaAutores extends Component {
+
+    constructor() {
+        super();
+        this.state = {lista : []};
+    }
+
+    componentDidMount() {
+        // Executa após o render ser executado
+        $.ajax({
+            url:"http://cdc-react.herokuapp.com/api/autores",
+            dataType: 'json',
+            success:function(resposta){
+                this.setState({lista:resposta}); // chama o render novamente
+            }.bind(this)
+        });
+    }
+    
+    componentWillMount() {
+        // Executa antes do render ser executado
+    }
+
+    render() {
+        return (
+            <div>
+                <fieldset>
+                    <legend>Author's List</legend>            
+                    <table className="pure-table pure-u-5-5">
+                        <thead>
+                            <tr>
+                                <th width="75%">Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.lista.map(function(autor) {
+                                return (
+                                    <tr key={autor.id}>
+                                        <td>{autor.nome}</td>                
+                                        <td>{autor.email}</td>                
+                                    </tr>
+                                );
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </fieldset>
+            </div>
+        );
+    }
+}
