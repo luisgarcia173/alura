134db574a2cde06f948569d6d04b07133b15cb68
Luis Garcia <luisgarcia173@gmail.com>
2018-02-27 21:26:07 -0300

    Consumindo uma API

--- alurapic-angular-II/client/app/app.component.ts
+++ alurapic-angular-II/client/app/app.component.ts
@@ -1,5 +1,6 @@
 //Imports
 import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 //Decorator
 @Component({
@@ -7,4 +8,22 @@ import {Component} from '@angular/core';
     selector: 'app',
     templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    //Attributes
+    fotos: Object[] = [];
+
+    //Constructor
+    constructor(http:Http){
+
+        //GET
+        http
+            .get('v1/fotos')
+            .map(res => res.json())
+            .subscribe(fotos => {
+                this.fotos = fotos;
+                console.log(this.fotos);
+            }, erro => console.log(erro));
+    }
+
+}
--- alurapic-angular-II/client/app/app.module.ts
+++ alurapic-angular-II/client/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
+import {HttpModule} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 //Decorator
 @NgModule({
-    imports: [ BrowserModule, FotoModule ],
+    imports: [ BrowserModule, FotoModule, HttpModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
