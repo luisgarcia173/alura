45485ab3da13ac696207138cee1d2aaf5153be34
Luis Garcia <luisgarcia@plusoft.com.br>
2018-05-04 10:43:17 -0300

    CAP III: Adicionando cartoes x usuario no localStorage

--- progressive-web-app/client/js/mural/Mural.js
+++ progressive-web-app/client/js/mural/Mural.js
@@ -1,11 +1,20 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = (JSON.parse(localStorage.getItem("cartoes")) || []).map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
+    let cartoes = pegaCartoesUsuario()
     cartoes.forEach(cartao => preparaCartao(cartao))
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
 
     render()
     Filtro.on("filtrado", render)
+
+    function pegaCartoesUsuario(){
+        let cartoesLocal = JSON.parse(localStorage.getItem(usuario))
+        if(cartoesLocal){
+            return cartoesLocal.map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
+        } else {
+            return []
+        }
+    }
 
     function preparaCartao(cartao){
         cartao.on("mudanca.**", salvaCartoes)
@@ -18,10 +27,20 @@ const Mural = (function(_render, Filtro){
     }
 
     function salvaCartoes (){
-        localStorage.setItem("cartoes", JSON.stringify(
+        localStorage.setItem(usuario, JSON.stringify(
             cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
         ))
     }
+
+    login.on("login", ()=>{
+        cartoes = pegaCartoesUsuario()
+        render()
+    })
+  
+    login.on("logout", ()=>{
+        cartoes = []
+        render()
+    })
 
     function adiciona(cartao){
         if (logado) {
