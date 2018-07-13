9ddef333e34f33b9b7610b78afda7370ce4046e6
Luis Garcia <luisgarcia@plusoft.com.br>
2018-07-13 13:43:33 -0300

    Sub-modulos

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, 
-        PhotosComponent, 
-        FilterByDescriptionPipe, 
-        LoadButtonComponent 
-    ],
     imports: [ 
-        HttpClientModule,
-        CommonModule //diretivas do angular
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule { }
